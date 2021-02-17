73bdc29afe677cc9af3023efd70dd5ffa62f9549
Matthieu <matthieu.labaune@le-campus-numerique.fr>
2021-02-17 20:58:45 +0100

    [UPD] · Single Post page issue #1

--- resources/views/front/post.blade.php
+++ resources/views/front/post.blade.php
@@ -1,110 +1,153 @@
 @extends('front.layout')
 
 @section('content')
-
-    <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-        <div class="max-w-xl sm:mx-auto lg:max-w-2xl">
-            <div class="flex flex-col mb-16 sm:text-center sm:mb-0">
-                <a href="/" class="mb-6 sm:mx-auto">
-                    <div class="flex items-center justify-center w-12 h-12 rounded-full bg-indigo-50">
-                        <svg class="w-10 h-10 text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
-                            <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none"
-                                     points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
-                        </svg>
+    <main>
+        <section class="relative block" style="height: 500px;">
+            <div class="absolute top-0 w-full h-full bg-center bg-cover"
+                 style='background-image: url("https://images.unsplash.com/photo-1499336315816-097655dcfbda?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=2710&amp;q=80");'>
+                <span id="blackOverlay" class="w-full h-full absolute opacity-50 bg-black"></span>
+            </div>
+        </section>
+        <section class="relative py-16 bg-white">
+            <div class="container w-3/5">
+                <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64">
+                    <div class="flex flex-wrap justify-center">
+                        <div class="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center">
+                            <div class="relative">
+                                <img alt="..." src="https://cdn.auth0.com/blog/illustrations/laravel.png"
+                                     class="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16"
+                                     style="max-width: 150px;"/>
+                            </div>
+                        </div>
+                        <div class="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
+                            <div class="py-6 px-3 mt-32 sm:mt-0">
+                                @foreach ($post->categories as $category)
+                                    <a href="{{ route('category', $category->slug) }}"
+                                       class="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1"
+                                       type="button" style="transition: all 0.15s ease 0s;">
+                                        {{ $category->title }}
+                                    </a>
+                                @endforeach
+                            </div>
+                        </div>
+                        <div class="w-full lg:w-4/12 px-4 lg:order-1">
+                            <div class="flex justify-center py-4 lg:pt-4 pt-8">
+                                <div class="mr-4 p-3 text-center">
+                                        <span
+                                            class="text-xl font-bold block uppercase tracking-wide text-gray-700">22</span>
+                                    <span class="text-sm text-gray-500">Likes</span>
+                                </div>
+                                <div class="lg:mr-4 p-3 text-center">
+                                        <span
+                                            class="text-xl font-bold block uppercase tracking-wide text-gray-700">89</span>
+                                    <span class="text-sm text-gray-500">Comments</span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </a>
-                <div class="max-w-xl md:mx-auto sm:text-center lg:max-w-2xl md:mb-3">
-                    <span class="relative text-7xl">{{ $post->title }}</span>
-                    <p class="text-base text-gray-700 md:text-lg">
-                        {{$post->excerpt}}
-                    </p>
-                </div>
-
-                <div class="flex flex-col justify-center mt-2">
-                    <p class="text-lg font-bold">{{ $post->user->name }}</p>
-                </div>
-
-                <div class="entry-cat-links meta-blk">
-                    <div class="cat-links">
-                        <span>@lang('In')</span>
-                        @foreach ($post->categories as $category)
-                            <a href="{{ route('category', $category->slug) }}">{{ $category->title }}</a>
-                        @endforeach
+                    <div class="text-center">
+                        <h3 class="text-4xl font-semibold leading-normal mb-2 text-gray-800 mb-2">
+                            {{ $post->title }}
+                        </h3>
                     </div>
-                    <span>@lang('On')</span>
-                    {{ formatDate($post->created_at) }}
-                </div>
 
-                <div class="entry-tags meta-blk">
-                    <span class="tagtext">@lang('Tags')</span>
-                    @foreach($post->tags as $tag)
-                        <a href="{{ route('tag', $tag->slug) }}">{{ $tag->tag }}</a>
-                    @endforeach
+                    <div class="text-center">
+                        <h3 class="text-sm font-semibold leading-normal mb-2 text-gray-800 mb-2">
+                            Rédigé le : {{ formatDate($post->created_at) }}
+                        </h3>
+                        <div class="text-sm leading-normal mt-0 mb-2 text-gray-500 font-bold uppercase">
+                            @foreach($post->tags as $tag)
+                                <a class="mr-3" href="{{ route('tag', $tag->slug) }}"> {{ $tag->tag }} </a>
+                            @endforeach
+                        </div>
+                    </div>
+
+                    <div class="mt-10 py-10 border-gray-300 text-center">
+                        <div class="flex flex-wrap justify-center">
+                            <div class="w-full lg:w-9/12 px-4">
+                                <p class="mb-4 text-lg leading-relaxed text-gray-800">
+                                    {{$post->excerpt}}
+                                </p>
+                            </div>
+                                <div class="px-4 py-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl">
+                                    <div class="s-content__entry-content">
+                                        {!! $post->body !!}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
 
-    <div class="px-4 py-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl">
-        <div class="s-content__entry-content">
-            {!! $post->body !!}
-        </div>
-    </div>
+        </section>
 
-    <!-- comments
-================================================== -->
-    <div class="comments-wrap">
 
-        <div id="comments" class="row">
-            <div id="commentsList" class="column large-12">
-                @if($post->valid_comments_count > 0)
-                    <div id="forShow">
-                        <p id="showbutton" class="text-center">
-                            <a id="showcomments" href="{{ route('posts.comments', $post->id) }}"
-                               class="btn h-full-width">@lang('Show comments')</a>
-                        </p>
-                        <p id="showicon" class="h-text-center" hidden>
-                            <span class="fa fa-spinner fa-pulse fa-3x fa-fw"></span>
-                        </p>
-                    </div>
-                @endif
+        <!-- comments
+    ================================================== -->
+        <section class="bg-white">
+            <div class="container w-3/5">
+                <div
+                    class="flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg">
+                    <div class="px-6">
+                        <div class="flex flex-wrap justify-center">
+                            <div class="comments-wrap">
+                                <div id="comments" class="row">
+                                    <div id="commentsList" class="column large-12">
+                                        @if($post->valid_comments_count > 0)
+                                            <div id="forShow">
+                                                <p id="showbutton" class="text-center">
+                                                    <a id="showcomments" href="{{ route('posts.comments', $post->id) }}"
+                                                       class="btn h-full-width">@lang('Show comments')</a>
+                                                </p>
+                                                <p id="showicon" class="h-text-center" hidden>
+                                                    <span class="fa fa-spinner fa-pulse fa-3x fa-fw"></span>
+                                                </p>
+                                            </div>
+                                        @endif
 
-            </div>
-        </div>
+                                    </div>
+                                </div>
 
-        @if(Auth::check())
-            <div class="row comment-respond">
-                <div id="respond" class="column">
-                    <h3>@lang('Add Comment')
-                        <span id="forName"></span>
-                        <span><a id="abort" hidden href="#">@lang('Abort reply')</a></span>
-                    </h3>
-                    <div id="alert" class="alert-box" style="display: none">
-                        <p></p>
-                        <span class="alert-box__close"></span>
-                    </div>
-                    <form id="messageForm" method="post" action="{{ route('posts.comments.store', $post->id) }}"
-                          autocomplete="off">
-                        <input id="commentId" name="commentId" type="hidden" value="">
-                        <div class="message form-field">
+                                @if(Auth::check())
+                                    <div class="row comment-respond">
+                                        <div id="respond" class="column">
+                                            <h3>@lang('Add Comment')
+                                                <span id="forName"></span>
+                                                <span><a id="abort" hidden href="#">@lang('Abort reply')</a></span>
+                                            </h3>
+                                            <div id="alert" class="alert-box" style="display: none">
+                                                <p></p>
+                                                <span class="alert-box__close"></span>
+                                            </div>
+                                            <form id="messageForm" method="post"
+                                                  action="{{ route('posts.comments.store', $post->id) }}"
+                                                  autocomplete="off">
+                                                <input id="commentId" name="commentId" type="hidden" value="">
+                                                <div class="message form-field">
                             <textarea name="message" id="message" class="h-full-width"
                                       placeholder="@lang('Your Message')"></textarea>
+                                                </div>
+                                                <br>
+                                                <p id="forSubmit" class="text-center">
+                                                    <input name="submit" id="submit"
+                                                           class="btn btn-primary btn-wide btn--large h-full-width"
+                                                           value="@lang('Add Comment')" type="submit">
+                                                </p>
+                                                <p id="commentIcon" class="h-text-center" hidden>
+                                                    <span class="fa fa-spinner fa-pulse fa-3x fa-fw"></span>
+                                                </p>
+                                            </form>
+                                        </div>
+                                    </div>
+                                @endif
+                            </div>
                         </div>
-                        <br>
-                        <p id="forSubmit" class="text-center">
-                            <input name="submit" id="submit" class="btn btn-primary btn-wide btn--large h-full-width"
-                                   value="@lang('Add Comment')" type="submit">
-                        </p>
-                        <p id="commentIcon" class="h-text-center" hidden>
-                            <span class="fa fa-spinner fa-pulse fa-3x fa-fw"></span>
-                        </p>
-                    </form>
+                    </div>
                 </div>
             </div>
-        @endif
-
-    </div>
-
+        </section>
+    </main>
 @endsection
 
 @section('scripts')
@@ -193,7 +236,7 @@
                     message: message.value
                 };
 
-                if(document.querySelector('#commentId').value != '') {
+                if (document.querySelector('#commentId').value != '') {
                     datas['commentId'] = commentId.value;
                 }
 
@@ -218,14 +261,14 @@
                 // Manage response
                 if (response.ok) {
                     purge();
-                    if(data == 'ok') {
+                    if (data == 'ok') {
                         showComments();
                         showAlert('success', '@lang('Your comment has been saved')');
                     } else {
                         showAlert('info', "@lang('Thanks for your comment. It will appear when an administrator has validated it. Once you are validated your other comments immediately appear.')");
                     }
                 } else {
-                    if(response.status == 422) {
+                    if (response.status == 422) {
                         showAlert('error', data.errors.message[0]);
                     } else {
                         errorAlert();
@@ -233,7 +276,7 @@
                 }
             }
 
-            const errorAlert = () =>  Swal.fire({
+            const errorAlert = () => Swal.fire({
                 icon: 'error',
                 title: '@lang('Whoops!')',
                 text: '@lang('Something went wrong!')'
@@ -304,9 +347,9 @@
             // Listener wrapper
             const wrapper = (selector, type, callback, condition = 'true', capture = false) => {
                 const element = document.querySelector(selector);
-                if(element) {
+                if (element) {
                     document.querySelector(selector).addEventListener(type, e => {
-                        if(eval(condition)) {
+                        if (eval(condition)) {
                             callback(e);
                         }
                     }, capture);
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -19,20 +19,23 @@
     <meta name="author" content="{{ isset($post) ? $post->user->name : __(config('app.author')) }}">
 
     <!-- Fonts -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-    <style>
-        @import url('https://fonts.googleapis.com/css?family=Karla:400,700&display=swap');
-
-        .font-family-karla {
-            font-family: karla;
-        }
-    </style>
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800" rel="stylesheet">
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/creativetimofficial/tailwind-starter-kit/tailwind.css">
+    <!-- Icons -->
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js" charset="utf-8" defer></script>
     <!-- Tailwind -->
     <link href="https://unpkg.com/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
     <!-- AlpineJS -->
